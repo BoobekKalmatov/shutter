2d3b30a00f0951f57028f10f8faf8933691fd19b
Boobek Kalmatov <boobekkalmatov8@gmail.com>
2023-10-09 00:31:26 +0300

    finish layout component login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import {Route, Routes} from 'react-router-dom'
 import './App.css';
+import Login from './components/Login/Login';
 import Navigation from './components/Navigation/Navigation';
 import Profile from './components/Profile/Profile';
+import Register from './components/Register/Register';
 
 function App() {
   return (
     <>
 
       <Routes>
-
         <Route path='/' element={<Navigation/>}/>
         <Route path='/profile' element={<Profile/>}/>
-
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
 
     </>
