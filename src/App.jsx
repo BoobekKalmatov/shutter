617e647d3667c3cff78b1b1d62b554764ddf2edb
Boobek Kalmatov <boobekkalmatov8@gmail.com>
2023-10-10 18:01:33 +0300

    problem fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,22 @@ import Navigation from './components/Navigation/Navigation';
 import Profile from './components/Profile/Profile';
 import Register from './components/Register/Register';
 
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
-import Profile from "./components/Profile/Profile";
-import Layout from "./components/Layout";
-
-import Login from "./components/Login/Login";
-import Register from "./components/Register/Register";
 
 function App() {
   return (
     <>
       <Layout>
-          <Routes>
-            <Route path="/" element={<Navigation />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
+        <Routes>
+          <Route path='/' element={<Navigation/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/lenta' element={<Lenta/>}/>
+        </Routes>
       </Layout>
 
 
-      <Routes>
-        <Route path='/' element={<Navigation/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/lenta' element={<Lenta/>}/>
-      </Routes>
+     
 
     </>
   );
