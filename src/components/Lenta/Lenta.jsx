378325e0c0da802c5b92c58b012df40fa27bb196
Boobek Kalmatov <boobekkalmatov8@gmail.com>
2023-10-12 20:21:21 +0300

    add icon search in input

--- src/components/Lenta/Lenta.jsx
+++ src/components/Lenta/Lenta.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Lenta.module.css';
 import LentaImageContent from './LentaImageContent/LentaImageContent';
-
+import searchimg from '../../svg/iconsearch.svg';
 function Lenta() {
 
     
@@ -15,6 +15,7 @@ function Lenta() {
                 <form>
                     <div className={styles.searchForm}>
                         <input className={styles.searchLenta} type="text" placeholder='Search' />
+                        <img src={searchimg} alt={searchimg} className={styles.iconSearch} />
                     </div>
                 </form>
             </div>
